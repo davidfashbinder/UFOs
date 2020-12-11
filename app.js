db34faa985c8a2555aa608d157d32fbc06b57f7b
David Fashbinder <david.fashbinder@gmail.com>
2020-12-10 23:05:51 -0500

    updating app.js

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ function buildTable(data) {
         );
     });
 }
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if there was a date entered 
+    // If there was, filter the data using that date
+    if (date) {
+        //Apply the filter to the data and only keep those rows
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data, if any
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button to be clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
