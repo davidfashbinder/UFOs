e2442499aa38d8f2ebad71f4490351ae60f4f8dd
David Fashbinder <david.fashbinder@gmail.com>
2020-12-10 22:25:55 -0500

    adding organized workspace and updated app.js file

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+function buildTable(data) {
+    // Clear any existing data from the table
+    tbody.html("");
+    // Loop through each object in the data
+    // append a row & cells for each value in the row 
+    data.forEach((dataRow) => {
+        // Append a row to the table body
+        // tr = table row tag 
+        let row = tbody.append("tr");
+        // Loop through each field in the dataRow
+        // Add each value as a table cell
+        // td = table cell tag 
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        }
+        );
+    });
+}
